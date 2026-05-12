b6fe1142e4bd6e72be0276a1c2904de449424080
Nguyễn Phương <55752553+akamabu@users.noreply.github.com>
2026-05-13 06:08:06 +0700

    Docs: Document index.dart

--- packages/agent_dart_base/lib/agent/canisters/index.dart
+++ packages/agent_dart_base/lib/agent/canisters/index.dart
@@ -1,3 +1,4 @@
+/// Documents packages/agent_dart_base/lib/agent/canisters/index.dart module purpose, public surface, and usage context
 export 'asset.dart';
 export 'asset_idl.dart';
 export 'management.dart';
