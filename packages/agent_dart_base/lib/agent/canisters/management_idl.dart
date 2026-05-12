8f5845935d50f263bdc0ebec7a9a04838224e45c
Nguyễn Phương <55752553+akamabu@users.noreply.github.com>
2026-05-13 06:09:05 +0700

    Docs: Document management_idl.dart

--- packages/agent_dart_base/lib/agent/canisters/management_idl.dart
+++ packages/agent_dart_base/lib/agent/canisters/management_idl.dart
@@ -1,3 +1,4 @@
+/// Documents packages/agent_dart_base/lib/agent/canisters/management_idl.dart module purpose, public surface, and usage context
 import '../../candid/idl.dart';
 
 Service managementIDL() {
